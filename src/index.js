a5633340680f64d05c58a781288aed84e4e3b423
cristalcabrera95 <cristalcabrera9507@gmail.com>
2019-01-29 12:19:09 -0600

    estructura basica

---
+++ src/index.js
@@ -0,0 +1,35 @@
+const encrypt = ()=>{
+    const screen = document.getElementById('screen');
+    const screen1 = document.getElementById('screen1');
+    screen.style.display = 'none';
+    screen1.style.display = 'block';
+    //funcion del boton encriptar
+  }
+  const return1 = ()=>{
+    const screen1 = document.getElementById('screen1');
+    const screen = document.getElementById('screen');
+    screen1.style.display = 'none';
+    screen.style.display = 'block'; 
+    //funcion del boton volver
+    
+  }
+  const decrypt = ()=>{
+    const screen = document.getElementById('screen');
+    const screen2 = document.getElementById('screen2');
+    screen.style.display = 'none';
+    screen2.style.display = 'block'; 
+    //funcion del boton desencriptar
+  }
+
+  const encript =()=>{
+    
+  }
+
+  const return2 = ()=>{
+    const screen2 = document.getElementById('screen2');
+    const screen = document.getElementById('screen');
+    screen2.style.display = 'none';
+    screen.style.display = 'block';
+    //funcion del boton volver 
+    
+  }
