9882f125a077f8e526ee3f216669edd11132dd23
cristalcabrera95 <cristalcabrera9507@gmail.com>
2019-02-05 13:18:33 -0600

    se modifico el cipher, se agrego codigo para los espacios

--- src/cipher.js
+++ src/cipher.js
@@ -1,34 +1,38 @@
 window.cipher = {
-  encode: (string, offset) =>{
-    let textResult = '';
+  encode:(offset, string)=> {
+    let textResult = "";
    //Recorrer cada uno de los caracteres del string
     for (let i = 0; i < string.length; i++) {
+      if(string[i] === " "){
+				textResult += " ";
+			}else{
       let result = string[i];
       let charCode = string.charCodeAt(i);
         result = String.fromCharCode((charCode - 65 + offset) % 26 + 65);
    
       textResult += result;
-      console.log(textResult);
+      }
     }
    
     return textResult;
   }, 
   
-  decode:(string, offset) =>{
-    let textResult = '';
+  decode:(offset, string)=> {
+    let textResult = "";
    
     for (let i = 0; i < string.length; i++) {
+      if(string[i] === ' '){
+				textResult += ' ';
+			}else{
       let result = string[i];
       let charCode = string.charCodeAt(i);
         result = String.fromCharCode((charCode + 65 - offset) % 26 + 65);
    
       textResult += result;
-      console.log(textResult);
+      }
+      
     }
    
     return textResult;
   }
-  
-  
-  // ... 
 };
--- src/index.js
+++ src/index.js
@@ -1,23 +1,26 @@
-const encodeButton = () =>{
+const abc = () =>{
     //obtiene el numero de posiciones
-    const offset = parseInt(document.getElementById('numero').value);
+    const offset = parseInt(document.getElementById("numero").value);
     //obtiene el texto ingresado
-    const string = document.getElementById('text').value;
+    const string = document.getElementById("text").value;
     //retornar el rresultado del texto codificado
-    let mensajeFinal= window.cipher.encode(string, offset);
-     document.getElementById("textResult").value= mensajeFinal;
-}
+   window.cipher.encode(string, offset);
+     document.getElementById("textResult").value= window.cipher.encode(offset, string);
+};
+const textEncode= document.getElementById("encodeButton");
+textEncode.addEventListener("click", abc);
 
-const decodeButton = () =>{
+const dfg = () =>{
     //obtiene el numero de posiciones
-    const offset = parseInt(document.getElementById('numero').value);
+    const offset = parseInt(document.getElementById("numero").value);
     //obtiene el texto ingresado
-    const string = document.getElementById('text').value;
+    const string = document.getElementById("text").value;
     //retornar el resultado del texto decodificado
-    let mensajeFinal= window.cipher.decode(string, offset);
+    let mensajeFinal= window.cipher.decode(offset, string);
      document.getElementById("textResult").value= mensajeFinal;
-}
-
+};
+const textDecode= document.getElementById("decodeButton");
+textDecode.addEventListener("click", dfg);
     
 
 
